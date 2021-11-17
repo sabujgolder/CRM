9edad60f5c1d4c0b96bfe91430e351285ff1cc61
sabuj <sabujgolder9334@gmail.com>
2021-11-17 23:58:42 +0600

    updates

--- CRM/accounts/views.py
+++ CRM/accounts/views.py
@@ -3,18 +3,28 @@ from .models import *
 # Create your views here.
 def home(request):
 	all_orders = Order.objects.all()
+	all_customers = Customer.objects.all()
+
+
 
 	total = all_orders.count()
 
 	delivered = Order.objects.filter(status = 'Delivered').count()
 	pending =all_orders.filter(status = 'Pending').count()
 
-	# dict = {'total':total,'delivered':delivered,'pending':pending}
+	dict = {'total':total,'delivered':delivered,'orders':all_orders,
+			'pending':pending,'customers':all_customers}
 
-	return render(request,'accounts/dashboard.html',{'total':total,'delivered':delivered,'pending':pending})
+	return render(request,'accounts/dashboard.html',context=dict)
 
 def products(request):
 	all_products = Product.objects.all()
 	return render(request,'accounts/products.html',{'products':all_products})
-def customer(request):
-	return render(request,'accounts/customer.html')
+def customer(request,pk):
+
+	customer_data = Customer.objects.get(id=pk)
+
+	customer_orders = customer_data.order_set.all()
+	total_orders = customer_orders.count()
+	dict = {'customer_data':customer_data,'customer_orders':customer_orders,'total_orders':total_orders}
+	return render(request,'accounts/customer.html',context = dict)
